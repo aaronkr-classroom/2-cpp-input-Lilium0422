0e70f4a38ec98bf43c829fcfbf711a3ec008bc95
Lilium0422 <a63789492@gmail.com>
2024-03-15 12:34:53 +0900

    Update chp1ex4.cpp

--- ch1/chp1ex4.cpp
+++ ch1/chp1ex4.cpp
@@ -9,6 +9,7 @@ int main()
 		const std::string s = "a string";
 		std::cout << s << std::endl;
 		{
-		const std::string s = "another string";
-		std::cout << s << std::endl; }}
+			const std::string s = "another string";
+			std::cout << s << std::endl; };}
+	return 0;
 }
